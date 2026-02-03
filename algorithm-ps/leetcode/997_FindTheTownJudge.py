f267b984bced9a6d6d6cdf67cb9fd2e43a0edb7d
Dohyeon Won <dh1072005@gmail.com>
2026-02-03 11:26:47 +0900

    graph

---
+++ algorithm-ps/leetcode/997_FindTheTownJudge.py
@@ -0,0 +1,54 @@
+""" https://leetcode.com/problems/find-the-town-judge/description/?envType=problem-list-v2&envId=graph
+In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+
+The town judge trusts nobody.
+Everybody (except for the town judge) trusts the town judge.
+There is exactly one person that satisfies properties 1 and 2.
+You are given an array trust where trust[i] = [ai, bi] representing that the person labeled ai trusts the person labeled bi. If a trust relationship does not exist in trust array, then such a trust relationship does not exist.
+
+Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+
+ 
+
+Example 1:
+
+Input: n = 2, trust = [[1,2]]
+Output: 2
+Example 2:
+
+Input: n = 3, trust = [[1,3],[2,3]]
+Output: 3
+Example 3:
+
+Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+Output: -1
+ 
+
+Constraints:
+
+1 <= n <= 1000
+0 <= trust.length <= 104
+trust[i].length == 2
+All the pairs of trust are unique.
+ai != bi
+1 <= ai, bi <= n
+"""
+
+from typing import List
+
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        # 1번부터 n번까지 있으므로 n+1 크기의 리스트 생성
+        scores = [0] * (n + 1)
+        
+        for a, b in trust:
+            scores[a] -= 1  # 내가 누군가를 믿으면 감점
+            scores[b] += 1  # 누군가 나를 믿어주면 가점
+            
+        for i in range(1, n + 1):
+            if scores[i] == n - 1:
+                return i
+                
+        return -1
