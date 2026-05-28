5c5e649b2ac8f8f88451373b9fb9b175d04338b9
Dohyeon Won <108811134+1Dohyeon@users.noreply.github.com>
2026-05-28 10:21:51 +0900

    two pointer

--- algorithm-ps/leetcode/150interview/26.remove-duplicates-from-sorted-array.py
+++ algorithm-ps/leetcode/150interview/26.remove-duplicates-from-sorted-array.py
@@ -60,8 +60,9 @@ class Solution:
 
         k = 1
         for i in range(1, len(nums)):
-            if nums[i] != nums[i-1]:
+            if nums[i] != nums[k-1]:
                 nums[k] = nums[i]
                 k += 1
 
         return k
+
